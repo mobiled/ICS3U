4ac18947f7465d8eb49a73f4d0e24a3fda34461c
Ahmed Addo <ahmed.addo243@gmail.com>
2017-02-21 11:21:58 -0500

    Commit Message

---
+++ HelloWorld/src/addo/HelloWorld.java
@@ -0,0 +1,14 @@
+package addo;
+/**
+ * HelloWorld
+ * Displays "Hello World"
+ * 21/02/17
+ * @author Ahmed Addo
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
