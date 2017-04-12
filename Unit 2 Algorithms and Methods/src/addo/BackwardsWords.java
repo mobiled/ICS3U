0205b03e30fb23eeb0b7bb8298451207a42f3487
Ahmed Addo <ahmed.addo243@gmail.com>
2017-04-12 11:26:01 -0400

    Backwords Sentence Included

---
+++ Unit 2 Algorithms and Methods/src/addo/BackwardsWords.java
@@ -0,0 +1,36 @@
+package addo;
+
+import java.util.Scanner;
+
+public class BackwardsWords {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a sentence");
+		String[] sentence = sc.nextLine().split(" ");
+		String s = "";
+
+		String word;
+		for (int i = 0; i < sentence.length; i++) {
+			word = sentence[i];
+			for (int j = word.length(); j > 0; j--) {
+				if (((Character) (sentence[i].charAt(0))).toString().matches("[A-Z]")) {
+					if (j == word.length()) {
+						System.out.print(Character.toUpperCase(word.charAt(j - 1)));
+					} else {
+						System.out.print(Character.toLowerCase(word.charAt(j - 1)));
+					}
+				} else {
+					System.out.print(word.charAt(j));
+				}
+
+			}
+			System.out.print(" ");
+		}
+
+	}
+
+	// public static String backwards(String[] sentence) {
+
+	// }
+}
