95094d1dd895877d20ce7fb9a79c95b629d4d81a
Ahmed Addo <ahmed.addo243@gmail.com>
2017-06-16 10:16:54 -0400

    FInal Culminating Project for NXT Robots

--- Arbitrator Programs/src/addo/SenseWall.java
+++ Arbitrator Programs/src/addo/SenseWall.java
@@ -1,5 +1,11 @@
 package addo;
 
+/**
+ * SenseWall.java
+ * Senses a wall or object in its path and changes direction
+ * 15/06/17
+ * @author Ahmed Addo
+ */
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
@@ -10,7 +16,11 @@ import lejos.util.Delay;
 public class SenseWall implements Behavior {
 	private UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S4);
 	private boolean suppress = false;
+
 	@Override
+	/**
+	 * If the ultra sonic sensor finds an object/wall in the way, take control
+	 */
 	public boolean takeControl() {
 		if (sonic.getDistance() <= 25) {
 			return true;
@@ -19,9 +29,11 @@ public class SenseWall implements Behavior {
 	}
 
 	@Override
+	/**
+	 * The robot rotates its direction
+	 */
 	public void action() {
 		suppress = false;
-
 		Motor.B.backward();
 		Motor.C.stop();
 		Delay.msDelay(1000);
