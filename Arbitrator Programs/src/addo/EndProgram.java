db8164e3df5582d62dac9e0e4e198d1d0889e30b
Ahmed Addo <ahmed.addo243@gmail.com>
2017-06-16 11:39:35 -0400

    Final Culminating for NXT Robots

--- Arbitrator Programs/src/addo/EndProgram.java
+++ Arbitrator Programs/src/addo/EndProgram.java
@@ -1,10 +1,17 @@
 package addo;
-
+/**
+ * EndProgram.java
+ * Ends the program when the escape button is pressed
+ * 15/06/17
+ * @author Ahmed Addo
+ */
 import lejos.nxt.Button;
 import lejos.robotics.subsumption.Behavior;
 
 public class EndProgram implements Behavior{
-
+	/**
+	 * When the escape button is pressed, take control
+	 */
 	@Override
 	public boolean takeControl() {
 		if(Button.ESCAPE.isDown()){		
@@ -12,13 +19,17 @@ public class EndProgram implements Behavior{
 		}
 		return false;
 	}
-
+	/**
+	 * Exits the program
+	 */
 	@Override
 	public void action() {
 		System.exit(0);
 		
 	}
-
+	/**
+	 * no suppress
+	 */
 	@Override
 	public void suppress() {
 		
