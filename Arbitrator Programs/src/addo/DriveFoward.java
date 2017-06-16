7b43143e1dcef81ebcafa275da96a5b9c0243419
Ahmed Addo <ahmed.addo243@gmail.com>
2017-06-16 10:26:54 -0400

    Final Culminating for NXT Robot

--- Arbitrator Programs/src/addo/DriveFoward.java
+++ Arbitrator Programs/src/addo/DriveFoward.java
@@ -1,16 +1,27 @@
 package addo;
-
+/**
+ * DriveForward.java
+ * Drives forward unless another behavior is in control
+ * 15/06/17
+ * @author Ahmed Addo
+ */
 import lejos.nxt.Motor;
 import lejos.robotics.subsumption.Behavior;
 
 public class DriveFoward implements Behavior{
 	private boolean suppress=false;
 	@Override
+	/**
+	 * Lowest priority behavior, does action whenever nothing else is in control
+	 */
 	public boolean takeControl() {
 		return true;
 	}
 
 	@Override
+	/**
+	 * Drives forward and stops when another behavior takes control
+	 */
 	public void action() {
 		suppress=false;
 		Motor.B.forward();
